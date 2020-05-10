37e47685da2c022da4f231ceb018ce811811d4e6
Tyr Chen <tyr.chen@gmail.com>
2020-05-10 11:34:43 -0700

    fix fixture path for vscode run / console run (no need 'flutter test test')

--- test/fixture/fixture_reader.dart
+++ test/fixture/fixture_reader.dart
@@ -1,3 +1,9 @@
 import 'dart:io';
 
-String fixture(String name) => File('test/fixture/$name').readAsStringSync();
+String fixture(String name) {
+  final String path = 'fixture/$name';
+  if (FileSystemEntity.typeSync(path) != FileSystemEntityType.notFound) {
+    return File(path).readAsStringSync();
+  }
+  return File('test/$path').readAsStringSync();
+}
